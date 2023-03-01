41141594d77f7f83ec6be55f825f28d75671eb15
pabloortegaa <63413550+pabloortegaa@users.noreply.github.com>
2023-03-01 12:18:19 +0100

    train dataset test

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pytest
 from  everywhere.datasets.load_data import get_users_data
-from everywhere.utils.constants import DEFAULT_HEADER
+from everywhere.utils.constants import DEFAULT_USER_DF, DEFAULT_TRAIN_DF, DEFAULT_FRIENDS_DF
 
 
 def url_to_drive(url):
@@ -13,24 +13,30 @@ def url_to_drive(url):
 def test_get_user_data():
     """Downloads the users dataset and returns the url."""
     df=pd.read_csv( url_to_drive("https://drive.google.com/file/d/1DOz_YmB6VgLg3z6Xy9R6iYP6lVphK4o4/view?usp=share_link"))
-    for col_name in DEFAULT_HEADER:
+    for col_name in DEFAULT_USER_DF:
         assert col_name in df.columns
 
-
+'''
 def test_get_events_data():
     """Downloads the events dataset from drive and returns the url."""
     url = url_to_drive("https://drive.google.com/file/d/1KehUtKFzjqGOxHMFNO9tf-MR5QRR7H9J/view?usp=share_link")
     assert url
+'''
 
 def test_get_train_data():
     """Downloads the train dataset from drive and returns the url."""
-    url = url_to_drive("https://drive.google.com/file/d/1zAdnZ1oBMaGfQAlKdQhM47oYt2zEAGx8/view?usp=share_link")
-    assert url
+    df =pd.read_csv( url_to_drive("https://drive.google.com/file/d/1zAdnZ1oBMaGfQAlKdQhM47oYt2zEAGx8/view?usp=share_link"))
+    for col_name in DEFAULT_TRAIN_DF:
+        assert col_name in df.columns
 
 def test_get_user_friends_data():
     """Downloads the user_friends dataset from drive and returns the url."""
-    url = url_to_drive("https://drive.google.com/file/d/1-U8ZBxhjtXYXG51Tiu6N-mNbOTh51UjE/view?usp=share_link")
-    assert url
+    df = pd.read_csv( url_to_drive("https://drive.google.com/file/d/1-U8ZBxhjtXYXG51Tiu6N-mNbOTh51UjE/view?usp=share_link"))
+    for col_name in DEFAULT_FRIENDS_DF:
+        assert col_name in df.columns
+
+
+    assert df
 
 def test_get_event_attendees_data():
     """Downloads the event_attendees dataset from drive and returns the url."""
