6a42b17ad66295a225f1e155cc8f83b65f0e0a1f
pabloortegaa <63413550+pabloortegaa@users.noreply.github.com>
2023-03-23 13:29:56 +0100

    preprocessing and balanced data in databricks

--- examples/models/test_model.py
+++ examples/models/test_model.py
@@ -3,7 +3,11 @@
 # MAGIC #### Everywhere preprocessing
 
 # COMMAND ----------
-
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv  # load environment variables
+from imblearn.under_sampling import ClusterCentroids
+from collections import Counter
 from everywhere.datasets.load_data import (
     get_users_data,
     get_events_data,
@@ -18,3 +22,122 @@ from everywhere.datasets.feature_extractor import (
 )
 
 # COMMAND ----------
+
+
+# MAGIC %md #### Load Datasets
+
+# COMMAND ----------
+# load environment variables
+load_dotenv()  
+
+
+# COMMAND ----------
+
+# MAGIC %md #### Load Users Data
+
+# COMMAND ----------
+
+df_users = get_users_data()
+print(df_users.shape)
+df_users.head()
+
+# COMMAND ----------
+
+# MAGIC %md #### Load user Friends Data
+
+# COMMAND ----------
+
+df_user_friends = get_user_friends_data()
+print(df_user_friends.shape)
+df_user_friends.head()
+
+# COMMAND ----------
+
+# MAGIC %md #### Load Events Attendees Data
+
+# COMMAND ----------
+df_event_attendees = get_event_attendees_data()
+print(df_event_attendees.shape)
+df_event_attendees.head()
+
+# COMMAND ----------
+
+
+# MAGIC %md #### Load train Data
+
+# COMMAND ----------
+df_train = get_train_data()
+print(df_train.shape)
+df_train.head()
+
+# COMMAND ----------
+
+
+# MAGIC %md #### Load Events Data
+
+# COMMAND ----------
+df_events = get_events_data()
+print(df_events.shape)
+df_events.head()
+
+# COMMAND ----------
+
+# MAGIC %md #### Feature Engineering
+
+# COMMAND ----------
+df_train = get_friends_attendee_nums(df_train, df_user_friends, df_event_attendees)
+df_train
+
+# COMMAND ----------
+
+# COMMAND ----------
+df_train = get_event_attendee_nums(df_train, df_event_attendees)
+print(df_train.shape)
+df_train.head()
+
+# COMMAND ----------
+
+# COMMAND ----------
+
+df_events_clean = df_events.drop(
+    columns=["user_id", "start_time", "city", "state", "zip", "country", "lat", "lng"]
+)
+# Merge with events
+df_train = pd.merge(
+    df_train, df_events_clean, how="inner", left_on="event", right_on="event_id"
+)
+df_train = df_train.drop(columns=["invited_y", "timestamp", "yes", "maybe", "no"])
+df_train
+
+# COMMAND ----------
+
+
+# MAGIC %md #### Balance dataset with Cluster Centroid
+
+# COMMAND ----------
+data = df_train.copy()
+X = data.drop("interested", axis=1)
+y = data["interested"]
+print("Original dataset shape {}".format(Counter(y)))
+cc = ClusterCentroids(random_state=42)
+X_res, y_res = cc.fit_resample(X, y)
+print("Resampled dataset shape {}".format(Counter(y_res)))
+
+# COMMAND ----------
+
+# MAGIC %md #### Shuffle Data
+
+# COMMAND ----------
+
+final = pd.merge(X_res, y_res, left_index=True, right_index=True)
+final = final.iloc[np.random.permutation(final.index)].reset_index(drop=True)
+final
+
+# COMMAND ----------
+
+
+
+
+
+
+
